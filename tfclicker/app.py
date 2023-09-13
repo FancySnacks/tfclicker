f1e8c4d9be11c42b80b2e78ea0c7d7ec073d100c
FancySnacks <adrian.urbaniak1336@gmail.com>
2023-09-13 09:41:33 +0200

    Restructrization

---
+++ tfclicker/app.py
@@ -0,0 +1,25 @@
+import time
+
+from multiprocessing import Process
+
+from item import Item
+
+
+class Overlord:
+    """Represents a game session"""
+    def __init__(self):
+        self.weapons = Item("weapons", 0, 1.0, 1.0)
+        self.scrap = Item("scrap", 0, 0.0, 0.0)
+        self.reclaimed = Item("reclaimed", 0, 0.0, 0.0)
+        self.refined = Item("refined", 0, 0.0, 0.0)
+        self.keys = Item("keys", 0, 0.0, 0.0)
+
+    def begin(self):
+        p = Process(target=self.increment_score)
+        p.start()
+
+    def increment_score(self):
+        while True:
+            self.weapons.increment()
+            print(f"Weapons: {self.weapons.amount}")
+            time.sleep(self.weapons.gain_rate)
---
+++ tfclicker/item.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass, field
+
+
+@dataclass
+class Item:
+    """Represents a single currency item (scrap, rec, ref or a key)"""
+    name: str
+    amount: float
+    gain_rate: float
+    gain_amount: float
+
+    def increment(self):
+        self.amount += self.gain_amount
